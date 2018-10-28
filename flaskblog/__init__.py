17d0b191c472d1b35742a68a5219c641d606363d
dimitrie13 <dimitrie13@outlook.com>
2018-10-28 19:03:27 -0400

    Log in Route and Form Created with user loader from DB. Password Hashing. Route Restrictions.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0e8a31259c87a05ec217d1439b50486f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# database instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# allows users to only access account page after log in/ redirect if not logged in
+login_manager.login_view = 'login'
+# nice log In message with bootstrap
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -8,6 +9,19 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    # RegistrationForm  username validation
+    def validate_username(self, username):
+        # database query to find user
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username already exists. Please choose another one')
+    # RegistrationForm email validation
+    def validate_email(self, email):
+        # database query to find user
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email already exists. Please choose another one')
 
 
 class LoginForm(FlaskForm):
