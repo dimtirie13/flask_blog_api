0b4f987786115bb7284a00f1256d9d9f1c94f75f
dimitrie13 <dimitrie13@outlook.com>
2018-10-29 17:56:19 -0400

    create/update/delete completed

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 # to allow users to import images
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -54,4 +54,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email already exists. Please choose another one')
-# inside the create/delete/ post branch
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
